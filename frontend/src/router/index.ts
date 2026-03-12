26ba1ff3db3c130d8765e011698886f084c3081d
Ratchanon Bualeesornsakun <ratchanon.blssk@gmail.com>
2026-03-12 18:18:04 +0700

    เพิ่ม API สำหรับการลงทะเบียนผู้ใช้งานคร่าวๆ / ปรับหน้าแสดงผลในส่วน frontend

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import HomeView from "@/views/HomeView.vue";
+import RegisterView from "@/views/auth/RegisterView.vue";
+import LoginView from "@/views/auth/LoginView.vue";
+import DashboardView from "@/views/auth/DashboardView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,12 +13,19 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/AboutView.vue"),
+      path: "/register",
+      name: "register",
+      component: RegisterView,
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: LoginView,
+    },
+    {
+      path: "/dashboard",
+      name: "dashboard",
+      component: DashboardView,
     },
   ],
 });
